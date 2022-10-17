a5d5b289ffbee018165211a8c0a21e2796a129fd
Mirac Seref <dev.miracseref@gmail.com>
2022-10-17 10:00:29 +0300

    app finished

---
+++ script.js
@@ -0,0 +1,21 @@
+const hourHand = document.querySelector(".hour-hand");
+const minuteHand = document.querySelector(".min-hand");
+const secondHand = document.querySelector(".second-hand");
+
+function setTime() {
+  const now = new Date();
+
+  const hours = now.getHours();
+  const minutes = now.getMinutes();
+  const seconds = now.getSeconds();
+
+  const hoursDegrees = (hours / 12) * 360 + 90;
+  const minutesDegrees = (minutes / 60) * 360 + 90;
+  const secondsDegrees = (seconds / 60) * 360 + 90;
+
+  hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+  minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
+  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+}
+
+setInterval(setTime, 1000);
